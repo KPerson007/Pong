ea1b96dc219e6f0d02be15130f870026cd4cef89
Kevin Traw <ktraw2@gmail.com>
2016-08-14 15:13:02 -0700

    Added a main menu screen and delayed the start of ball movement every time the game starts or resets.

--- src/PongCanvas.java
+++ src/PongCanvas.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.net.URL;
 import java.util.*;
 
 /**
@@ -18,8 +19,11 @@ public class PongCanvas extends Canvas implements Runnable, KeyListener {
     private final int ABS_DEFAULT_DELTA_Y_FULL = 10;
     private final int PADDLE_MOVING_ADDITIONAL_X = 3;
     private final int DEFAULT_DELTA_X = 12;
+    private final int START_DELAY_MS = 500;
 
     Random random = new Random();
+    Image menuImage = null;
+    private int delayTimer = -FRAME_DELAY; //it is not 0 because the game logic adds FRAME_DELAY to delayTimer before any calls to Thread.sleep()
     private Thread runThread;
     private int leftBoxY = 0;
     private int rightBoxY = 0;
@@ -31,12 +35,14 @@ public class PongCanvas extends Canvas implements Runnable, KeyListener {
     private boolean moveBall = true;
     private boolean leftMoving = false;
     private boolean rightMoving = false;
+    private boolean isInMenu = true;
     private boolean[] noCollide = {false, false, false, false, false, false};
     private int p1Score = 0;
     private int p2score = 0;
 
     public void resetGame()
     {
+        delayTimer = -FRAME_DELAY;
         Dimension d = this.getSize();
         leftBoxY = (this.getSize().height / 2) - (BOX_HEIGHT / 2);
         rightBoxY = leftBoxY;
@@ -122,20 +128,38 @@ public class PongCanvas extends Canvas implements Runnable, KeyListener {
             runThread = new Thread(this);
             runThread.start();
         }
-
-        g.fillRect(BOX_SPACE_FROM_WALL, leftBoxY, ELEMENT_WIDTH, BOX_HEIGHT); //draw left paddle
-        g.fillRect(d.width - ELEMENT_WIDTH - BOX_SPACE_FROM_WALL, rightBoxY, ELEMENT_WIDTH, BOX_HEIGHT); //draw right paddle
-        g.drawLine(d.width / 2, 0, d.width / 2, d.height); //draw dividing line
-        g.fillOval(ball.x, ball.y, ELEMENT_WIDTH - 5, ELEMENT_WIDTH - 5); //draw pong ball
-        int p1TextX = d.width / 4;
-        int p2TextX = (d.width /2) + (d.width / 4);
-        g.drawString("" + p1Score, p1TextX, 10); //player 1 score
-        g.drawString("" + p2score, p2TextX, 10); //player 2 score
-        String p1KeyPrompt = "W = Up; S = Down";
-        String p2KeyPrompt = "Up Arrow = Up; Down Arrow = Down";
-        g.drawString(p1KeyPrompt, p1TextX - (g.getFontMetrics().stringWidth(p1KeyPrompt) / 2), 25); //player 1 key prompt
-        if (twoPlayer)
-            g.drawString(p2KeyPrompt, p2TextX - (g.getFontMetrics().stringWidth(p2KeyPrompt) / 2), 25); //player 2 key prompt, only draw if 2 player game
+        if (!isInMenu)
+        {
+            g.fillRect(BOX_SPACE_FROM_WALL, leftBoxY, ELEMENT_WIDTH, BOX_HEIGHT); //draw left paddle
+            g.fillRect(d.width - ELEMENT_WIDTH - BOX_SPACE_FROM_WALL, rightBoxY, ELEMENT_WIDTH, BOX_HEIGHT); //draw right paddle
+            g.drawLine(d.width / 2, 0, d.width / 2, d.height); //draw dividing line
+            g.fillOval(ball.x, ball.y, ELEMENT_WIDTH - 5, ELEMENT_WIDTH - 5); //draw pong ball
+            int p1TextX = d.width / 4;
+            int p2TextX = (d.width / 2) + (d.width / 4);
+            g.drawString("" + p1Score, p1TextX, 10); //player 1 score
+            g.drawString("" + p2score, p2TextX, 10); //player 2 score
+            String p1KeyPrompt = "W = Up; S = Down";
+            String p2KeyPrompt = "Up Arrow = Up; Down Arrow = Down";
+            g.drawString(p1KeyPrompt, p1TextX - (g.getFontMetrics().stringWidth(p1KeyPrompt) / 2), 25); //player 1 key prompt
+            if (twoPlayer)
+                g.drawString(p2KeyPrompt, p2TextX - (g.getFontMetrics().stringWidth(p2KeyPrompt) / 2), 25); //player 2 key prompt, only draw if 2 player game
+        }
+        else
+        {
+            if (menuImage == null)
+            {
+                try
+                {
+                    URL imagePath = PongCanvas.class.getResource("Pong-Menu.png");
+                    menuImage = Toolkit.getDefaultToolkit().getImage(imagePath);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            g.drawImage(menuImage, 0, 0, d.width, d.height, this);
+        }
     }
 
     @Override
@@ -144,136 +168,140 @@ public class PongCanvas extends Canvas implements Runnable, KeyListener {
         while(true)
         {
             Dimension d = this.getSize();
-            for (int k : keysPressed) //move paddles if keys are pressed
+            for (int k : keysPressed) //handle key presses
             {
                 if (DEBUG)
                     System.out.println(k);
-                switch (k)
-                {
-                    case KeyEvent.VK_W:
-                        if (!(leftBoxY - MOVE_OFFSET <= 0))
-                        {
-                            leftMoving = true;
-                            leftBoxY -= MOVE_OFFSET;
+                switch (k) {
+                    case KeyEvent.VK_W: //move left paddle up
+                        if (!isInMenu) {
+                            if (!(leftBoxY - MOVE_OFFSET <= 0)) {
+                                leftMoving = true;
+                                leftBoxY -= MOVE_OFFSET;
+                            } else
+                                leftBoxY = 0;
                         }
-                        else
-                            leftBoxY = 0;
                         break;
-                    case KeyEvent.VK_S:
-                        if (!(leftBoxY + BOX_HEIGHT + MOVE_OFFSET >= d.height))
-                        {
-                            leftMoving = true;
-                            leftBoxY += MOVE_OFFSET;
+                    case KeyEvent.VK_S: //move left paddle down
+                        if (!isInMenu) {
+                            if (!(leftBoxY + BOX_HEIGHT + MOVE_OFFSET >= d.height)) {
+                                leftMoving = true;
+                                leftBoxY += MOVE_OFFSET;
+                            } else
+                                leftBoxY = d.height - BOX_HEIGHT;
                         }
-                        else
-                            leftBoxY = d.height - BOX_HEIGHT;
                         break;
-                    case KeyEvent.VK_UP:
-                        if (!(rightBoxY - MOVE_OFFSET <= 0) && twoPlayer)
-                        {
-                            rightMoving = true;
-                            rightBoxY -= MOVE_OFFSET;
+                    case KeyEvent.VK_UP: //move right paddle up
+                        if (!isInMenu) {
+                            if (!(rightBoxY - MOVE_OFFSET <= 0) && twoPlayer) {
+                                rightMoving = true;
+                                rightBoxY -= MOVE_OFFSET;
+                            } else
+                                rightBoxY = 0;
                         }
-                        else
-                            rightBoxY = 0;
                         break;
-                    case KeyEvent.VK_DOWN:
-                        if (!(rightBoxY + BOX_HEIGHT + MOVE_OFFSET >= d.height) && twoPlayer)
-                        {
-                            rightMoving = true;
-                            rightBoxY += MOVE_OFFSET;
+                    case KeyEvent.VK_DOWN: //move right paddle down
+                        if (!isInMenu) {
+                            if (!(rightBoxY + BOX_HEIGHT + MOVE_OFFSET >= d.height) && twoPlayer) {
+                                rightMoving = true;
+                                rightBoxY += MOVE_OFFSET;
+                            } else
+                                rightBoxY = d.height - BOX_HEIGHT;
                         }
-                        else
-                            rightBoxY = d.height - BOX_HEIGHT;
+                        break;
+                    case KeyEvent.VK_1: //start a one player game, TODO: implement one player games
+                        break;
+                    case KeyEvent.VK_2: //start a two player game
+                        isInMenu = false;
+                        delayTimer = -FRAME_DELAY;
                         break;
                 }
             }
-
-            //move ball
-            if (moveBall)
+            if (!isInMenu)
             {
-                int newX = deltaBall.x;
-                int newY = deltaBall.y;
-                if (DEBUG)
+                if (delayTimer < START_DELAY_MS) //give a delay before the game starts running
+                    delayTimer += FRAME_DELAY;
+                else
                 {
-                    System.out.println("x: " + newX);
-                    System.out.println("y: " + newY);
+                    //move ball
+                    if (moveBall) {
+                        int newX = deltaBall.x;
+                        int newY = deltaBall.y;
+                        if (DEBUG) {
+                            System.out.println("x: " + newX);
+                            System.out.println("y: " + newY);
+                        }
+                        ball.x += newX;
+                        ball.y += newY;
+                    }
+
+                    //check collisions
+                    if (ball.x <= ELEMENT_WIDTH + BOX_SPACE_FROM_WALL && ball.y + ELEMENT_WIDTH >= leftBoxY && ball.y <= leftBoxY + BOX_HEIGHT && noCollide[CollisionType.LEFT_PADDLE] == false) //check collision with left box
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Left Box");
+                        deltaBall.x = DEFAULT_DELTA_X + getDeltaVariation();
+                        if (leftMoving)
+                            deltaBall.x += PADDLE_MOVING_ADDITIONAL_X;
+                        deltaBall.y = getNewDeltaY(leftBoxY) + getDeltaVariation();
+                        System.out.println("new X: " + deltaBall.x + " new Y: " + deltaBall.y);
+                        //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
+                        noCollide[CollisionType.LEFT_PADDLE] = true;
+                        noCollide[CollisionType.RIGHT_PADDLE] = false;
+                        noCollide[CollisionType.TOP] = false;
+                        noCollide[CollisionType.BOTTOM] = false;
+                    } else if (ball.x + ELEMENT_WIDTH >= d.width - ELEMENT_WIDTH - BOX_SPACE_FROM_WALL && ball.y + ELEMENT_WIDTH >= rightBoxY && ball.y <= rightBoxY + BOX_HEIGHT && noCollide[CollisionType.RIGHT_PADDLE] == false) //check collision with right box
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Right Box");
+                        deltaBall.x = -DEFAULT_DELTA_X + getDeltaVariation();
+                        if (rightMoving)
+                            deltaBall.x -= PADDLE_MOVING_ADDITIONAL_X;
+                        deltaBall.y = getNewDeltaY(rightBoxY) + getDeltaVariation();
+                        System.out.println("new X: " + deltaBall.x + " new Y: " + deltaBall.y);
+                        //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
+                        noCollide[CollisionType.LEFT_PADDLE] = false;
+                        noCollide[CollisionType.RIGHT_PADDLE] = true;
+                        noCollide[CollisionType.TOP] = false;
+                        noCollide[CollisionType.BOTTOM] = false;
+                    } else if (ball.x <= 0) //player 1 loses
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Left Wall");
+                        p2score++;
+                        resetGame();
+                    } else if (ball.x + ELEMENT_WIDTH >= d.width) //player 2 loses
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Right Wall");
+                        p1Score++;
+                        resetGame();
+                    } else if (ball.y <= 0 && noCollide[CollisionType.TOP] == false) //check collisions with top
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Top Or Bottom");
+                        deltaBall.y = -deltaBall.y + getDeltaVariation();
+                        //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
+                        noCollide[CollisionType.LEFT_PADDLE] = false;
+                        noCollide[CollisionType.RIGHT_PADDLE] = false;
+                        noCollide[CollisionType.TOP] = true;
+                        noCollide[CollisionType.BOTTOM] = false;
+                    } else if (ball.y + ELEMENT_WIDTH >= d.height && noCollide[CollisionType.BOTTOM] == false) //check collision with bottom
+                    {
+                        if (DEBUG)
+                            System.out.println("Ball Collide With Top Or Bottom");
+                        deltaBall.y = -deltaBall.y + getDeltaVariation();
+                        //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
+                        noCollide[CollisionType.LEFT_PADDLE] = false;
+                        noCollide[CollisionType.RIGHT_PADDLE] = false;
+                        noCollide[CollisionType.TOP] = false;
+                        noCollide[CollisionType.BOTTOM] = true;
+                    }
+
+                    leftMoving = false;
+                    rightMoving = false;
                 }
-                ball.x += newX;
-                ball.y += newY;
             }
-
-            //check collisions
-            if (ball.x <= ELEMENT_WIDTH + BOX_SPACE_FROM_WALL && ball.y + ELEMENT_WIDTH >= leftBoxY && ball.y <= leftBoxY + BOX_HEIGHT && noCollide[CollisionType.LEFT_PADDLE] == false) //check collision with left box
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Left Box");
-                deltaBall.x = DEFAULT_DELTA_X + getDeltaVariation();
-                if (leftMoving)
-                    deltaBall.x += PADDLE_MOVING_ADDITIONAL_X;
-                deltaBall.y = getNewDeltaY(leftBoxY) + getDeltaVariation();
-                System.out.println("new X: " + deltaBall.x + " new Y: " + deltaBall.y);
-                //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
-                noCollide[CollisionType.LEFT_PADDLE] = true;
-                noCollide[CollisionType.RIGHT_PADDLE] = false;
-                noCollide[CollisionType.TOP] = false;
-                noCollide[CollisionType.BOTTOM] = false;
-            }
-            else if (ball.x + ELEMENT_WIDTH >= d.width - ELEMENT_WIDTH - BOX_SPACE_FROM_WALL && ball.y + ELEMENT_WIDTH >= rightBoxY && ball.y <= rightBoxY + BOX_HEIGHT && noCollide[CollisionType.RIGHT_PADDLE] == false) //check collision with right box
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Right Box");
-                deltaBall.x = -DEFAULT_DELTA_X + getDeltaVariation();
-                if (rightMoving)
-                    deltaBall.x -= PADDLE_MOVING_ADDITIONAL_X;
-                deltaBall.y = getNewDeltaY(rightBoxY) + getDeltaVariation();
-                System.out.println("new X: " + deltaBall.x + " new Y: " + deltaBall.y);
-                //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
-                noCollide[CollisionType.LEFT_PADDLE] = false;
-                noCollide[CollisionType.RIGHT_PADDLE] = true;
-                noCollide[CollisionType.TOP] = false;
-                noCollide[CollisionType.BOTTOM] = false;
-            }
-            else if (ball.x <= 0) //player 1 loses
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Left Wall");
-                p2score++;
-                resetGame();
-            }
-            else if (ball.x + ELEMENT_WIDTH >= d.width) //player 2 loses
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Right Wall");
-                p1Score++;
-                resetGame();
-            }
-            else if (ball.y <= 0 && noCollide[CollisionType.TOP] == false) //check collisions with top
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Top Or Bottom");
-                deltaBall.y = -deltaBall.y + getDeltaVariation();
-                //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
-                noCollide[CollisionType.LEFT_PADDLE] = false;
-                noCollide[CollisionType.RIGHT_PADDLE] = false;
-                noCollide[CollisionType.TOP] = true;
-                noCollide[CollisionType.BOTTOM] = false;
-            }
-            else if (ball.y + ELEMENT_WIDTH >= d.height && noCollide[CollisionType.BOTTOM] == false) //check collision with bottom
-            {
-                if (DEBUG)
-                    System.out.println("Ball Collide With Top Or Bottom");
-                deltaBall.y = -deltaBall.y + getDeltaVariation();
-                //make sure the ball doesn't get stuck by disallowing a double collision, but also reallow all other collisions
-                noCollide[CollisionType.LEFT_PADDLE] = false;
-                noCollide[CollisionType.RIGHT_PADDLE] = false;
-                noCollide[CollisionType.TOP] = false;
-                noCollide[CollisionType.BOTTOM] = true;
-            }
-
-            leftMoving = false;
-            rightMoving = false;
-
             repaint();
             try
             {
